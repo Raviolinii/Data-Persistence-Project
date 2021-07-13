c3363d38ba3396734bc4c3b773cd7265929fc261
Krzysztof Garlacz <54943478+Raviolinii@users.noreply.github.com>
2021-07-13 13:12:55 +0200

    Menu button and input field

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    private InputField inputField;
+    private  Button startButton;
+    private int characterLimit = 12;
+    private int requiredCharacters = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField = GetComponentInChildren<InputField>();
+        inputField.onValueChanged.AddListener(delegate {ButtonInteractableHandler();});
+        inputField.characterLimit = characterLimit;
+
+        startButton = GetComponentInChildren<Button>();
+        startButton.interactable = false;
+        startButton.onClick.AddListener(delegate {OnStartClick();});
+    }
+    public bool NameLength()
+    {
+        if (inputField.text.Length >= requiredCharacters)
+            return true;
+        else
+            return false;
+    }
+    public void ButtonInteractableHandler()
+    {
+        if (NameLength())
+            startButton.interactable = true;
+        else
+            startButton.interactable = false;
+    }
+    public void OnStartClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
